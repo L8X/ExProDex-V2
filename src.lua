342931f5e67cbf5e22fd9211eb56398cfbabeecd
Madi <49875377+L8X@users.noreply.github.com>
2022-07-05 14:19:45 +0100

    Update src.lua

--- src.lua
+++ src.lua
@@ -49,36 +49,6 @@ local Workspace = cloneref(Workspace)
 
 pcall(function() settings().Diagnostics.IsScriptStackTracingEnabled = false end)
 
-local old_print = print
-local old_warn = warn
-local old_error = error
-
-local print = old_print
-local warn = old_warn
-local error = old_error
-
-task.spawn(function()
-pcall(function()
-if clonefunction and getgenv then
-getgenv().print = clonefunction(old_print)
-getgenv().warn = clonefunction(old_warn)
-getgenv().error = clonefunction(old_error)
-print = clonefunction(old_print)
-warn = clonefunction(old_warn)
-error = clonefunction(old_error)
-end
-end)
-end)
-
-local TestService = cloneref(game:GetService("TestService"))
-
-local function info(args)
-local args_string = tostring(args)
-return TestService:Message(args_string)
-end
-
-getgenv().info = info
-
 local wait = function(int)
 if not int then
    int = 0
@@ -221,10 +191,96 @@ task.spawn(function()
     end)
 end)
 Folder.Parent = HideInMe
+
+task.spawn(function()
+pcall(function()
+if cloneref and checkcaller and hookfunction and hookmetamethod and getnamecallmethod then
+
+local Content = cloneref(game:GetService("ContentProvider"));
+local CoreGui = cloneref(game:GetService("CoreGui"));
+local randomizedCoreGuiTable;
+local randomizedGameTable;
+
+local coreguiTable = {}
+
+game:GetService("ContentProvider"):PreloadAsync({CoreGui}, function(assetId)
+    if not assetId:find("rbxassetid://") then
+        table.insert(coreguiTable, assetId);
+end
+end)
+local gameTable = {}
+
+for i, v in pairs(game:GetDescendants()) do
+    if v:IsA("ImageLabel") then
+        if v.Image:find('rbxassetid://') and v:IsDescendantOf(CoreGui) then else
+            table.insert(gameTable, v.Image)
+        end
+    end
+end
+
+function randomizeTable(t)
+    local n = #t
+    while n > 0 do
+        local k = math.random(n)
+        t[n], t[k] = t[k], t[n]
+        n = n - 1
+    end
+    return t
+end
+
+local ContentProviderBypass
+ContentProviderBypass = hookmetamethod(game, "__namecall", function(self, Instances, ...)
+    local method = getnamecallmethod();
+    local args = ...;
+
+    if not checkcaller() and (method == "preloadAsync" or method == "PreloadAsync") then
+        if Instances and Instances[1] and self.ClassName == "ContentProvider" then
+            if Instances ~= nil then
+                if typeof(Instances[1]) == "Instance" and (table.find(Instances, CoreGui) or table.find(Instances, game)) then
+                    if Instances[1] == CoreGui then
+                        randomizedCoreGuiTable = randomizeTable(coreguiTable)
+                        return ContentProviderBypass(self, randomizedCoreGuiTable, ...)
+                    end
+
+                    if Instances[1] == game then
+                        randomizedGameTable = randomizeTable(gameTable)
+                        return ContentProviderBypass(self, randomizedGameTable, ...)
+                    end
+                end
+            end
+        end
+    end
+
+    return ContentProviderBypass(self, Instances, ...)
+end)
+
+local preloadBypass; preloadBypass = hookfunction(Content.PreloadAsync, function(a, b, c)
+    if not checkcaller() then
+        if typeof(a) == "Instance" and tostring(a) == "ContentProvider" and typeof(b) == "table" then
+            if (table.find(b, CoreGui) or table.find(b, game)) and not (table.find(b, true) or table.find(b, false)) then
+                if b[1] == CoreGui then
+                    randomizedCoreGuiTable = randomizeTable(coreguiTable)
+                    return preloadBypass(a, randomizedCoreGuiTable, c)
+                end
+                if b[1] == game then
+                    randomizedGameTable = randomizeTable(gameTable)
+                    return preloadBypass(a, randomizedGameTable, c)
+                end
+            end
+        end
+    end
+
+    return preloadBypass(a, b, c)
+end)
+
+end
+end)
+end)
 				
 local LogService = cloneref(game:GetService("LogService"))
 local ScriptContext = cloneref(game:GetService("ScriptContext"))
 
+--[[
 task.spawn(function()
 pcall(function()
 if getconnections then	
@@ -240,6 +296,7 @@ end
 end
 end)
 end)
+]]--
 
 task.spawn(function()
 pcall(function()
